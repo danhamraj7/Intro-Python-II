b499f4549ce14ba02655ea33a444755bcae7fee4
danhamraj7 <ddan_7@hotmail.com>
2020-07-08 15:45:56 -0400

    created Player and Room class and imported them into adv.py

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,14 +39,16 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player('Dan', room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
+# * Prints the current description (the textwrap module might be
+# useful here).
 # * Waits for user input and decides what to do.
 #
-# If the user enters a cardinal direction, attempt to move to the room there.
+# If the user enters a cardinal direction, attempt to move to the
+# room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
