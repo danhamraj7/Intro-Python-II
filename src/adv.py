e4fab45409e71f526a7ffd0227c064c02f72d578
danhamraj7 <ddan_7@hotmail.com>
2020-07-08 21:39:41 -0400

    removed the dictionary from room and updated player and adv files

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+import textwrap
 
 # Declare all the rooms
 
@@ -25,22 +26,51 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].routes["n"] = room['foyer']
-room['foyer'].routes["s"] = room['outside']
-room['foyer'].routes["n"] = room['overlook']
-room['foyer'].routes["e"] = room['narrow']
-room['overlook'].routes["s"] = room['foyer']
-room['narrow'].routes["w"] = room['foyer']
-room['narrow'].routes["n"] = room['treasure']
-room['treasure'].routes["s"] = room['narrow']
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
 
 #
 # Main
 #
+# tries to move the player in a specific direction
+
+
+def try_direction(player, direction):
+    # this will check the player current location, and when the player
+    # enter a first_char to make a move it would check to see if there is
+    # a room in that specific direction. they are moved to that room else
+    #  if there are no room print a message saying "You cannot go there"
+    # and the player is not moved.
+    attribute = direction + '_to'
+
+    # Python 'hasattr' allows us to check if a class has an attribute
+    if hasattr(player.location, attribute):
+        # this checks if the entry was a valid direction.
+        # 'getattr' fetches the value associated with the attribute
+        player.location = getattr(player.location, attribute)  # fetch
+        # the player old location with the attribute and update it to the new
+        # location base on the entry the player entered.
+
 
 # Make a new player object that is currently in the 'outside' room.
 player = Player('Dan', room['outside'])
+
 # Write a loop that:
+
+while True:
+
+    #
+    # * Prints the current room name
+    # * Prints the current description (the textwrap module might be
+    # useful here).
+    print("\n")
+    print(player.location)
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be
@@ -52,3 +82,20 @@ player = Player('Dan', room['outside'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+user_is_playing = True
+
+while user_is_playing:
+    print(my_player.current_room.name)
+    for line in textwrap.wrap(my_player.current_room.description):
+        # this returns a list of strings so we use a for loop
+        print(line)
+
+        user_input = input("which direction would you like to go? (n/e/w/s)")
+
+        if user_input in ["n", "e", "w", "s"]:
+            my_player.move(user_input)
+        else:
+            print("You existed the game Thanks for playing")
+
+        user_is_playing = False
--- src/room.py
+++ src/room.py
@@ -2,12 +2,9 @@
 # This should have name and description attributes.
 
 class Room:
-    def __init__(self, name, description, n_to=None, e_to=None, s_to=None, w_to=None,):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.routes = {
-            "n": n_to,
-            "e": n_to,
-            "s": n_to,
-            "w": n_to,
-        }
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
